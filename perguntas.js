1528e9183ed3475d2fe8382ca1cd3b9cdf00b246
Gabriela2009-gif <gabriela.peske@escola.pr.gov.br>
2025-08-18 11:00:48 -0300

    perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -15,7 +15,7 @@ criaCartao(
     'O que é evolução?',
     'A evolução é o processo de mudança nas características hereditárias das populações de organismos ao longo das gerações.'
 )
-    criaCartao(
+criaCartao(
     'Programação',
     'Qual é a base da programação?',
     'A programação funciona a partir do pensamento lógico.'
